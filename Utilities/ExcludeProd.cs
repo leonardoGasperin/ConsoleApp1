cd391f2019f8b6a96d8c30ff0113a250ed198e6e
LeonardoGasperin <gasperinlvg@gmail.com>
2022-10-14 17:20:27 -0300

    fix something really strange

--- Utilities/ExcludeProd.cs
+++ Utilities/ExcludeProd.cs
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-﻿using ConsoleApp2;
-using ConsoleApp2.Utilities;
-=======
-﻿using ConsoleApp1.Utilities;
-using ConsoleApp1;
->>>>>>> cac31cf11a5dc2505a910d20ca615ba3ca15cc52
+﻿using ConsoleApp2.Utilities;
+using ConsoleApp2;
 
 internal class ExcludeProd
 {
@@ -16,11 +11,7 @@ internal class ExcludeProd
             produto.Exclude(produto.Code);
         else
             Exclude();
-<<<<<<< HEAD
-        
-=======
 
->>>>>>> cac31cf11a5dc2505a910d20ca615ba3ca15cc52
         UserEnter.ConsoleInput();
     }
 }
